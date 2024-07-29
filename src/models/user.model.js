1e1b58001135b218f6a292f289d70f808d4c59ce
ManishSingh10 <112544875+ManishSingh10@users.noreply.github.com>
2024-07-29 17:34:08 +0530

    Jwt and bcrpt authentication

---
+++ src/models/user.model.js
@@ -0,0 +1,107 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true //it makes searching fast..based on index
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String, // cloudinary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+
+//hashing password before saving it to database
+userSchema.pre('save', async function (next) {
+
+    if (!this.isModified("password")) return next(); //check password change hua hai ke nhi agar hua hai toh he password hash karenge.
+    this.password = bcrypt.hash(this.password, 10); //second para. is salt number
+    next();
+
+
+})
+
+
+//now, compare hash password and plain password ... with the help of custom method
+userSchema.methods.isPasswordCorrect = async function (password) {
+
+    return await bcrypt.compare(password, this.password) // 1st-> user password, 2nd-> hash password stored in DB.-> it will return true or false.
+}
+
+
+userSchema.methods.generateAcessToken = function () {
+
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+userSchema.methods.generateRefreshToken = function () {
+
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+export const User = mongoose.model("User", userSchema)
